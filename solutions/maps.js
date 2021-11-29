a93f6fdd71dc7d36c4445f7b1786cfde94dd7772
Cullen Sharp <cullen.sharp100@gmail.com>
2021-11-28 17:01:06 -0800

    Expand on maps

--- solutions/maps.js
+++ solutions/maps.js
@@ -3,11 +3,28 @@
  */
 
 const ages = {
-  boris: 55,
-  julia: 23,
-  ann: 56,
+  Boris: 55,
+  Julia: 23,
+  Ann: 56,
 };
 
 for (person in ages) {
   console.log(`${person} is ${ages[person]} years old.`);
+}
+
+// Javascript comes with a map class to make this easy
+const newAges = new Map();
+newAges.set("Jean", 65);
+newAges.set("Sean", 34);
+newAges.set("Gene", 21);
+
+console.log("\n");
+
+console.log(`Is Jean's age known? ${newAges.has("Jean") ? "Yes." : "No."}`);
+console.log(`Sean is ${newAges.get("Sean")} years old.`);
+
+console.log("\n");
+
+for (guy of newAges) {
+  console.log(`${guy[0]} is ${guy[1]} years old.`);
 }
