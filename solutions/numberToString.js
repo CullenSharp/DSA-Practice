0bbd8b9293f36b68ce3447498492c96ded989bc8
CullenSharp2 <cullen.sharp100@gmail.com>
2021-12-02 16:43:53 -0800

    Add numberToString

---
+++ solutions/numberToString.js
@@ -0,0 +1,35 @@
+/**
+ * Converts a given number to a given base
+ * @param {number} n
+ * @param {2|8|10} base
+ */
+function numberToString(n, base = 10) {
+  let result = "";
+  let sign = "";
+
+  if (n < 0) {
+    sign = "-";
+    n = -n;
+  }
+
+  do {
+    // Concatentates the result onto the previous iteration
+    result = String(n % base) + result;
+    n = Math.floor(n / base);
+  } while (n > 0);
+
+  switch (base) {
+    case 8:
+      result += "₈";
+      break;
+    case 10:
+      result += "₁₀";
+      break;
+    default:
+      break;
+  }
+
+  return sign + result;
+}
+
+module.exports = numberToString;
